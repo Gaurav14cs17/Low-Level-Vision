f65d9addb6b8f5fe3bd0680cc6dde4f3c36cd814
Gaurav14cs17 <gourav14cs17@gmail.com>
2023-03-08 00:09:25 +0530

    Low-Level-Vision loss

--- flare_removal_pytorch/dataloader/data_loader.py
+++ flare_removal_pytorch/dataloader/data_loader.py
@@ -13,30 +13,30 @@ import torch
 
 
 class RandomGammaCorrection(object):
-    def __init__(self, gamma = None):
+    def __init__(self, gamma=None):
         self.gamma = gamma
 
-    def __call__(self,image):
+    def __call__(self, image):
         if self.gamma == None:
             # more chances of selecting 0 (original image)
-            gammas = [0.5,1,2]
+            gammas = [0.5, 1, 2]
             self.gamma = random.choice(gammas)
             return TF.adjust_gamma(image, self.gamma, gain=1)
-        elif isinstance(self.gamma,tuple):
-            gamma=random.uniform(*self.gamma)
+        elif isinstance(self.gamma, tuple):
+            gamma = random.uniform(*self.gamma)
             return TF.adjust_gamma(image, gamma, gain=1)
         elif self.gamma == 0:
             return image
         else:
-            return TF.adjust_gamma(image,self.gamma,gain=1)
+            return TF.adjust_gamma(image, self.gamma, gain=1)
 
 
 def remove_background(image):
-    image =  np.float32(np.array(image))
+    image = np.float32(np.array(image))
     _EPS = 1e-7
-    rgb_max = np.max(image ,(0,1))
-    rgb_min = np.min(image ,(0,1))
-    image = (image - rgb_min)*rgb_max/(rgb_max - rgb_min + _EPS)
+    rgb_max = np.max(image, (0, 1))
+    rgb_min = np.min(image, (0, 1))
+    image = (image - rgb_min) * rgb_max / (rgb_max - rgb_min + _EPS)
     image = torch.from_numpy(image)
     return image
 
@@ -83,7 +83,6 @@ class Flare_Image_Loader(data.Dataset):
         base_img = gain * base_img
         base_img = torch.clamp(base_img, min=0, max=1)
 
-
         # load flare image
         flare_path = random.choice(self.flare_list)
         flare_img = Image.open(flare_path)
@@ -94,7 +93,6 @@ class Flare_Image_Loader(data.Dataset):
         flare_img = to_tensor(flare_img)
         flare_img = adjust_gamma(flare_img)
 
-
         if self.reflective_flag:
             reflective_img = to_tensor(reflective_img)
             reflective_img = adjust_gamma(reflective_img)
@@ -102,13 +100,11 @@ class Flare_Image_Loader(data.Dataset):
 
         flare_img = remove_background(flare_img)
 
-
         if self.transform_flare is not None:
             flare_img = self.transform_flare(flare_img)
 
         # change color
         flare_img = color_jitter(flare_img)
-
 
         # flare blur
         blur_transform = transforms.GaussianBlur(21, sigma=(0.1, 3.0))
@@ -116,13 +112,13 @@ class Flare_Image_Loader(data.Dataset):
         flare_img = flare_img + flare_DC_offset
         flare_img = torch.clamp(flare_img, min=0, max=1)
 
-
         # merge image
         merge_img = flare_img + base_img
         merge_img = torch.clamp(merge_img, min=0, max=1)
 
         if self.mask_type == None:
-            return adjust_gamma_reverse(base_img), adjust_gamma_reverse(flare_img), adjust_gamma_reverse(merge_img), gamma
+            return adjust_gamma_reverse(base_img), adjust_gamma_reverse(flare_img), adjust_gamma_reverse(
+                merge_img), gamma
         elif self.mask_type == "luminance":
             # calculate mask (the mask is 3 channel)
             one = torch.ones_like(base_img)
@@ -137,7 +133,8 @@ class Flare_Image_Loader(data.Dataset):
             threshold_value = 0.99 ** gamma
             flare_mask = torch.where(merge_img > threshold_value, one, zero)
 
-        return adjust_gamma_reverse(base_img), adjust_gamma_reverse(flare_img), adjust_gamma_reverse(merge_img), flare_mask, gamma
+        return adjust_gamma_reverse(base_img), adjust_gamma_reverse(flare_img), adjust_gamma_reverse(
+            merge_img), flare_mask, gamma
 
     def __len__(self):
         return len(self.data_list)
@@ -171,10 +168,13 @@ class Flare_Image_Loader(data.Dataset):
         print("Now we have", len(self.reflective_list), 'refelctive flare images')
 
 
-
 if __name__ == '__main__':
+    import os
+
     data_path = "D:/labs/LOW_LEVEL_IMAGEING/Low-Level-Vision/data/Flare7K_dataset/"
+    output_path = "D:/labs/LOW_LEVEL_IMAGEING/Low-Level-Vision/data/Flare7K_dataset/train/"
     from matplotlib import pyplot as plt
+
     transform_base = transforms.Compose([transforms.RandomCrop((512, 512), pad_if_needed=True, padding_mode='reflect'),
                                          transforms.RandomHorizontalFlip(),
                                          transforms.RandomVerticalFlip()
@@ -187,13 +187,18 @@ if __name__ == '__main__':
                                           transforms.RandomVerticalFlip()
                                           ])
 
-    flare_image_loader = Flare_Image_Loader( data_path + 'Flickr24K', transform_base, transform_flare)
+    flare_image_loader = Flare_Image_Loader(data_path + 'Flickr24K', transform_base, transform_flare)
     flare_image_loader.load_scattering_flare('Flare7K', data_path + 'Flare7k/Scattering_Flare/Compound_Flare')
     flare_image_loader.load_reflective_flare('Flare7K', data_path + 'Flare7k/Reflective_Flare')
-    img_index = 267
-    test_base_img, test_flare_img, test_merge_img, flare_mask = flare_image_loader[img_index]
 
-    plt.imsave("test_merge_img.png" , test_merge_img.permute(1, 2, 0).cpu().numpy())
-    plt.imsave("test_base_img.png" ,test_base_img.permute(1, 2, 0).cpu().numpy())
-    plt.imsave("test_flare_img.png", test_flare_img.permute(1, 2, 0).cpu().numpy())
-    print("done")
+
+    print(len(flare_image_loader))
+    for img_index in range(len(flare_image_loader)):
+        test_base_img, test_flare_img, test_merge_img, flare_mask = flare_image_loader[img_index]
+        gt_path = os.path.join(output_path, "GT", "GT_" + str(img_index) + ".png"  )
+        in_path = os.path.join(output_path, "IN", "IN_" + str(img_index) + ".png")
+        plt.imsave(in_path, test_merge_img.permute(1, 2, 0).cpu().numpy())
+        plt.imsave(gt_path, test_base_img.permute(1, 2, 0).cpu().numpy())
+        print("done: " , img_index )
+        break
+
--- flare_removal_tensorflow/dataloader/dataloader.py
+++ flare_removal_tensorflow/dataloader/dataloader.py
@@ -1,94 +1,164 @@
 import os.path
 from typing import Iterable, Tuple, Union
 import tensorflow as tf
+
 _SHUFFLE_BUFFER_SIZE = 10_000
 
 
-def image_dataset_from_files(data_dir,image_shape,batch_size = 0,shuffle = True,repeat = -1):
-  extensions = ['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG']
-  globs = [os.path.join(data_dir, f'*.{e}') for e in extensions]
-  globs += [os.path.join(data_dir, '**', f'*.{e}') for e in extensions]
-  files = tf.data.Dataset.list_files(globs, shuffle, seed=0)
-  def _parser(file_name):
-    blob = tf.io.read_file(file_name)
-    image = tf.io.decode_image(blob, dtype=tf.float32)
-    image.set_shape(image_shape)
-    return image
-  images = files.map(_parser, num_parallel_calls=tf.data.AUTOTUNE, deterministic=not shuffle)
-  if repeat < 0:
-    images = images.repeat()
-  elif repeat > 0:
-    images = images.repeat(repeat)
-  if batch_size > 0:
-    images = images.batch(batch_size, drop_remainder=True)
-  images = images.prefetch(tf.data.AUTOTUNE)
-  return images
+class DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self):
+        self.dir_path = ""
+        self.bit_depth = 8
+        self.input_shape=(640, 640, 3)
+        self.batch_size = 3
+        self.images_dir_list = self.get_images_name()
+
+    def get_images_name(self):
+        return os.listdir(os.path.join(self.dir_path , "GT"))
+
+    def load_images(self, path):
+        path = tf.io.read_file(path)
+        image = tf.io.decode_image(path, dtype=tf.uint8)
+        image = tf.cast(image, dtype=tf.float32)
+        image = image / (2 ** self.bit_depth - 1)
+        image = tf.expand_dims(image, 0)
+        return image
+
+    def get_random_crop(self, value, size, seed=None, name=None):
+        tf_value = tf.convert_to_tensor(value, name="value")
+        tf_size = tf.convert_to_tensor(size, dtype=tf.int32, name='size')
+        value_shape = tf.shape(tf_value)
+        offset = tf.random.uniform(tf.shape(value_shape), dtype=tf_size.dtype, maxval=tf_size.dtype.max, seed=seed) % (value_shape - tf_size + 1)
+        offset_floor_mod = tf.math.floormod(offset, tf.stack([1, 2, 2, 1]))
+        diff = offset - offset_floor_mod
+        crop_value = tf.slice(value, diff, tf_size, name=name)
+        return crop_value
+
+    def get_patch(self , gt_image, in_image , num_patches , patch_size , gt_filter , in_filter):
+        patches_list = list()
+        image_cat = tf.concat([gt_image , in_image] ,-1)
+        for _ in range(num_patches):
+            crop_image = self.get_random_crop(image_cat ,[1,patch_size,patch_size , in_filter + gt_filter])
+            patches_list.append(crop_image)
+        all_crop_image = tf.concat(patches_list ,0)
+        return all_crop_image
+
+    def __getitem__(self, index):
+        if (index + 1) * self.batch_size > len(self.images_dir_list):
+            self.batch_size = len(self.images_dir_list) - index * self.batch_size
+
+        files_batch = self.images_dir_list[index * self.batch_size: (index + 1) * self.batch_size]
+        IN_images = []
+        GT_images = []
+        for image_name in files_batch:
+            gt_name = "GT"+ image_name.split('_')[-1]
+            in_name = "IN" + image_name.split('_')[-1]
+            gt_full_path = os.path.join(self.dir_path , "GT" , gt_name)
+            in_full_path = os.path.join(self.dir_path , "IN" , in_name)
+            in_image = self.load_images(in_full_path)
+            gt_image = self.load_images(gt_full_path)
+
+            GT_image , IN_image = self.get_patch(gt_image , in_image , num_patches=4 , patch_size=256 , gt_filter=3 , in_filter=3)
+            for x , y in zip(IN_image , GT_image):
+                IN_images.append(x)
+                IN_images.append(y)
+        return IN_images, IN_images
+
+    def __len__(self):
+        return int(len(self.images_dir_list)/float(self.batch_size))
 
 
-def image_dataset_from_tfrecords(globs,tag,image_shape,batch_size = 0,shuffle = True,repeat = -1):
-
-  files = tf.data.Dataset.list_files(globs, shuffle, seed=0)
-  examples = files.interleave(tf.data.TFRecordDataset,num_parallel_calls=tf.data.AUTOTUNE,deterministic=not shuffle)
-  if shuffle:
-    examples = examples.shuffle(buffer_size=_SHUFFLE_BUFFER_SIZE, seed=0, reshuffle_each_iteration=True)
-
-  def _parser(example):
-    features = tf.io.parse_single_example(example, features={tag: tf.io.FixedLenFeature([], tf.string)})
-    image_u8 = tf.reshape(tf.io.decode_raw(features[tag], tf.uint8), image_shape)
-    image_f32 = tf.image.convert_image_dtype(image_u8, tf.float32)
-    return image_f32
-
-  images = examples.map(_parser, num_parallel_calls=tf.data.AUTOTUNE, deterministic=not shuffle)
-  if repeat < 0:
-    images = images.repeat()
-  elif repeat > 0:
-    images = images.repeat(repeat)
-  if batch_size > 0:
-    images = images.batch(batch_size, drop_remainder=True)
-  images = images.prefetch(tf.data.AUTOTUNE)
-  return images
 
 
-def get_scene_dataset(path,source,batch_size,input_shape = (640, 640, 3),repeat = 0):
-  """Returns scene images according to configuration."""
-  if source == 'tfrecord':
-    return image_dataset_from_tfrecords(
-        globs=os.path.join(path, '*.tfrecord'),
-        tag='image',
-        image_shape=input_shape,
-        batch_size=batch_size,
-        repeat=repeat)
-
-  elif source == 'jpg':
-    return image_dataset_from_files(
-        data_dir=path,
-        image_shape=input_shape,
-        batch_size=batch_size,
-        repeat=repeat)
-
-  else:
-    raise ValueError('Unrecognized data source', source)
 
 
-def get_flare_dataset(path,source,batch_size,input_shape = (752, 1008, 3),repeat = -1):
-  """Returns flare images according to configuration."""
-  if source == 'tfrecord':
-    return image_dataset_from_tfrecords(
-        globs=path,
-        tag='flare',
-        image_shape=input_shape,
-        batch_size=batch_size,
-        repeat=repeat)
 
-  elif source == 'jpg':
-    return image_dataset_from_files(
-        data_dir=path,
-        image_shape=input_shape,
-        batch_size=batch_size,
-        repeat=repeat)
+def image_dataset_from_files(data_dir, image_shape, batch_size=0, shuffle=True, repeat=-1):
+    extensions = ['jpg', 'jpeg', 'png', 'JPG', 'JPEG', 'PNG']
+    globs = [os.path.join(data_dir, f'*.{e}') for e in extensions]
+    globs += [os.path.join(data_dir, '**', f'*.{e}') for e in extensions]
+    files = tf.data.Dataset.list_files(globs, shuffle, seed=0)
 
-  else:
-    raise ValueError('Unrecognized data source', source)
+    def _parser(file_name):
+        blob = tf.io.read_file(file_name)
+        image = tf.io.decode_image(blob, dtype=tf.float32)
+        image.set_shape(image_shape)
+        return image
+
+    images = files.map(_parser, num_parallel_calls=tf.data.AUTOTUNE, deterministic=not shuffle)
+    if repeat < 0:
+        images = images.repeat()
+    elif repeat > 0:
+        images = images.repeat(repeat)
+    if batch_size > 0:
+        images = images.batch(batch_size, drop_remainder=True)
+    images = images.prefetch(tf.data.AUTOTUNE)
+    return images
+
+
+def image_dataset_from_tfrecords(globs, tag, image_shape, batch_size=0, shuffle=True, repeat=-1):
+    files = tf.data.Dataset.list_files(globs, shuffle, seed=0)
+    examples = files.interleave(tf.data.TFRecordDataset, num_parallel_calls=tf.data.AUTOTUNE, deterministic=not shuffle)
+    if shuffle:
+        examples = examples.shuffle(buffer_size=_SHUFFLE_BUFFER_SIZE, seed=0, reshuffle_each_iteration=True)
+
+    def _parser(example):
+        features = tf.io.parse_single_example(example, features={tag: tf.io.FixedLenFeature([], tf.string)})
+        image_u8 = tf.reshape(tf.io.decode_raw(features[tag], tf.uint8), image_shape)
+        image_f32 = tf.image.convert_image_dtype(image_u8, tf.float32)
+        return image_f32
+
+    images = examples.map(_parser, num_parallel_calls=tf.data.AUTOTUNE, deterministic=not shuffle)
+    if repeat < 0:
+        images = images.repeat()
+    elif repeat > 0:
+        images = images.repeat(repeat)
+    if batch_size > 0:
+        images = images.batch(batch_size, drop_remainder=True)
+    images = images.prefetch(tf.data.AUTOTUNE)
+    return images
+
+
+def get_scene_dataset(path, source, batch_size, input_shape=(640, 640, 3), repeat=0):
+    """Returns scene images according to configuration."""
+    if source == 'tfrecord':
+        return image_dataset_from_tfrecords(
+            globs=os.path.join(path, '*.tfrecord'),
+            tag='image',
+            image_shape=input_shape,
+            batch_size=batch_size,
+            repeat=repeat)
+
+    elif source == 'jpg':
+        return image_dataset_from_files(
+            data_dir=path,
+            image_shape=input_shape,
+            batch_size=batch_size,
+            repeat=repeat)
+
+    else:
+        raise ValueError('Unrecognized data source', source)
+
+
+def get_flare_dataset(path, source, batch_size, input_shape=(752, 1008, 3), repeat=-1):
+    """Returns flare images according to configuration."""
+    if source == 'tfrecord':
+        return image_dataset_from_tfrecords(
+            globs=path,
+            tag='flare',
+            image_shape=input_shape,
+            batch_size=batch_size,
+            repeat=repeat)
+
+    elif source == 'jpg':
+        return image_dataset_from_files(
+            data_dir=path,
+            image_shape=input_shape,
+            batch_size=batch_size,
+            repeat=repeat)
+
+    else:
+        raise ValueError('Unrecognized data source', source)
 
 
 '''
@@ -96,4 +166,10 @@ def get_flare_dataset(path,source,batch_size,input_shape = (752, 1008, 3),repeat
 
  --scene_dir=/path/to/flare-free/
   --flare_dir=/path/to/flare-only/
+  
+  
+   scenes = data_provider.get_scene_dataset(
+      FLAGS.scene_dir, FLAGS.data_source, FLAGS.batch_size, repeat=FLAGS.epochs)
+  flares = data_provider.get_flare_dataset(FLAGS.flare_dir, FLAGS.data_source,
+                                           FLAGS.batch_size)
 '''
